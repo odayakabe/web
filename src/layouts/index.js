98a4b82dbff79f59e8779e96c03b611cfb5d4685
Andrey Galkin <andrey@futoin.org>
2018-02-03 01:41:40 +0200

    NEW: LD-JSON meta-info

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -12,16 +12,39 @@ import './index.scss'
 
 import GatsbyConfig from '../../gatsby-config'
 
+const json_ld_website = `
+{
+"@context": "http://schema.org/",
+"@type": "WebSite",
+"name": "FutoIn Guide",
+"url": "https://futoin.org/"
+}`;
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
       title={GatsbyConfig.siteMetadata.title}
       meta={[
         { name: 'description', content: 'FutoIn project website' },
-        { name: 'keywords', content: 'futoin, async, ci, ci, cid, asyncsteps, node.js, api, interfaces, security' },
+        { name: 'keywords', content: [
+            'futoin',
+            'async',
+            'ci',
+            'cd',
+            'cid',
+            'asyncsteps',
+            'node.js',
+            'api',
+            'interfaces',
+            'security',
+        ].join(', ')},
       ]}>
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     </Helmet>
+    <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{__html: json_ld_website}}>
+    </script>
     <RMWCProvider>
         <Navigation>
             {children()}
